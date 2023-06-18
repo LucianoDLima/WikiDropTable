2aa6744da48eaba0b38455219f0b559c18626b50
LucianoDLima <ljr.luciano.junior@outlook.com>
2023-06-18 11:20:11 -0300

    Changed identation to 4.

--- script/parameterTranslator.js
+++ script/parameterTranslator.js
@@ -2,33 +2,37 @@
  * Translates parameters in the input text based on the @parameterToBeTranslated object.
  * Ignores translation within the angle brackets ("<" and ">").
  */
-export const parameterTranslator = (inputDrops, outputDrops, paramaterToBeTranslated) => {
-  // Splits by both '<' and '>' via RegEx.
-  let output = inputDrops.value.split(/[<>]+/);
-  const outputLength = output.length;
+export const parameterTranslator = (
+    inputDrops,
+    outputDrops,
+    paramaterToBeTranslated
+) => {
+    // Splits by both '<' and '>' via RegEx.
+    let output = inputDrops.value.split(/[<>]+/);
+    const outputLength = output.length;
 
-  for (let parameter in paramaterToBeTranslated) {
-      // Insensitive case regex that matches any digits at the end of the string.
-      const regex = new RegExp(`\\b${parameter}\\b\\d*`, 'ig');
+    for (let parameter in paramaterToBeTranslated) {
+        // Insensitive case regex that matches any digits at the end of the string.
+        const regex = new RegExp(`\\b${parameter}\\b\\d*`, 'ig');
 
-      // Extract any digits at the end of the match and append them to the replacement string.
-      const digits = parameter.match(/\d+$/);
-      const translated =
-          paramaterToBeTranslated[parameter] + (digits ? digits[0] : '');
+        // Extract any digits at the end of the match and append them to the replacement string.
+        const digits = parameter.match(/\d+$/);
+        const translated =
+            paramaterToBeTranslated[parameter] + (digits ? digits[0] : '');
 
-      for (let i = 0; i < outputLength; i++) {
-          // Everything between angle brackets are on odd indexes inside 'output'.
-          if (i % 2 == 0) {
-              output[i] = output[i].replace(regex, translated);
-          }
-      }
-  }
+        for (let i = 0; i < outputLength; i++) {
+            // Everything between angle brackets are on odd indexes inside 'output'.
+            if (i % 2 == 0) {
+                output[i] = output[i].replace(regex, translated);
+            }
+        }
+    }
 
-  // Joins 'output' by alternating between angle brackets.
-  outputDrops.value = output.reduce((acc, curr, index) => {
-      if (index === 0) {
-          return curr;
-      }
-      return acc + (index % 2 === 0 ? '>' : '<') + curr;
-  }, '');
+    // Joins 'output' by alternating between angle brackets.
+    outputDrops.value = output.reduce((acc, curr, index) => {
+        if (index === 0) {
+            return curr;
+        }
+        return acc + (index % 2 === 0 ? '>' : '<') + curr;
+    }, '');
 };
