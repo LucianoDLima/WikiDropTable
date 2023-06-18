79b2aea54aa6f7845c3e580572040afa71f19655
LucianoDLima <ljr.luciano.junior@outlook.com>
2023-06-18 11:15:22 -0300

    Refactor for improved code readability.
    Moved paramaterTranslator function to separate file and also altered comments slightly.

--- script/script.js
+++ script/script.js
@@ -1,43 +1,9 @@
 import { fetchTranslatedName } from './items.js';
 import { dropTableHead } from './parameters.js';
+import { parameterTranslator } from './parameterTranslator.js';
 
 const inputDrops = document.querySelector('#input');
 const outputDrops = document.querySelector('#output');
-
-/**
- * Translates parameters in the input text based on the @dropTableHead object.
- * Ignores translation within these arrow thingys "<" and ">".
- */
-const parameterTranslator = (objectToBeTranslated) => {
-    // Splits by both '<' and '>' via RegEx.
-    let output = inputDrops.value.split(/[<>]+/);
-    const len = output.length;
-
-    for (let parameter in objectToBeTranslated) {
-        // Insensitive case regex that matches any digits at the end of the string.
-        const regex = new RegExp(`\\b${parameter}\\b\\d*`, 'ig');
-
-        // Extract any digits at the end of the match and append them to the replacement string.
-        const digits = parameter.match(/\d+$/);
-        const translated =
-            objectToBeTranslated[parameter] + (digits ? digits[0] : '');
-
-        for (let i = 0; i < len; i++) {
-            // Everything between any '<' and '>' are on odd indexes inside 'output'.
-            if (i % 2 == 0) {
-                output[i] = output[i].replace(regex, translated);
-            }
-        }
-    }
-
-    // Joins 'output' by alternating between '<' and '>'.
-    outputDrops.value = output.reduce((acc, curr, index) => {
-        if (index === 0) {
-            return curr;
-        }
-        return acc + (index % 2 === 0 ? '>' : '<') + curr;
-    }, '');
-};
 
 const handleFetchItems = (item, lines, i, start, end) => {
     if (item.indexOf(start) != -1) {
@@ -71,7 +37,7 @@ const translateItemNames = () => {
 inputDrops.addEventListener('input', () => {
     outputDrops.value = inputDrops.value;
 
-    parameterTranslator(dropTableHead);
+    parameterTranslator(inputDrops, outputDrops,dropTableHead)
     translateItemNames();
 });
 
