e5cb3ed77e5757bd039ecdbdb477cd0b85532369
LucianoDLima <ljr.luciano.junior@outlook.com>
2023-06-19 08:04:36 -0300

    Refactor: Moved itemTranslator function to its own file for better code readability.

---
+++ script/itemTranslator.js
@@ -0,0 +1,36 @@
+import { fetchTranslatedName } from "./items.js";
+
+export const translateItemNames = (outputDrops) => {
+  let lines = outputDrops.value.split('\n');
+  const symbolsToCheck = [
+      ['=', '|'],
+      ['[[', ']'],
+      ['Descobrir:', '|'],
+      ['[[File:', '.png]]']
+  ];
+
+  for (let i = 0; i < lines.length; i++) {
+      let item = lines[i];
+
+      // Check if it has a name to be translated.
+      symbolsToCheck.forEach(([start, end]) => {
+          handleFetchItems(item, lines, i, start, end);
+      });
+  }
+
+  outputDrops.value = lines.join('\n');
+};
+
+function handleFetchItems(item, lines, i, start, end, fetcher) {
+  if (item.indexOf(start) != -1) {
+    item = item.split(start);
+    item = item[1].split(end);
+    item = item[0].trim();
+
+    const translatedName = fetchTranslatedName(item.toLowerCase());
+    if (translatedName) {
+      // Replace the item name with the translated name.
+      lines[i] = lines[i].replace(item, translatedName);
+    }
+  }
+}
--- script/script.js
+++ script/script.js
@@ -1,51 +1,16 @@
-import { fetchTranslatedName } from './items.js';
 import { dropTableHead, skillNames, infoboxItem, infoboxRecipe } from './parameters.js';
 import { parameterTranslator } from './parameterTranslator.js';
+import { translateItemNames } from './itemTranslator.js';
 
 const inputDrops = document.querySelector('#input');
 const outputDrops = document.querySelector('#output');
-
-const handleFetchItems = (item, lines, i, start, end) => {
-    if (item.indexOf(start) != -1) {
-        item = item.split(start);
-        item = item[1].split(end);
-        item = item[0].trim();
-        
-        const translatedName = fetchTranslatedName(item.toLowerCase());
-        if (translatedName) {
-            // Replace the item name with the translated name.
-            lines[i] = lines[i].replace(item, translatedName);
-        }
-    }
-};
-
-const translateItemNames = () => {
-    let lines = outputDrops.value.split('\n');
-    const symbolsToCheck = [
-        ['=', '|'],
-        ['[[', ']'],
-        ['Descobrir:', '|'],
-        ['[[File:', '.png]]']
-    ];
-
-    for (let i = 0; i < lines.length; i++) {
-        let item = lines[i];
-
-        // Check if it has a name to be translated.
-        symbolsToCheck.forEach(([start, end]) => {
-            handleFetchItems(item, lines, i, start, end);
-        });
-    }
-
-    outputDrops.value = lines.join('\n');
-};
 
 
 inputDrops.addEventListener('input', () => {
     outputDrops.value = inputDrops.value;
 
     parameterTranslator(inputDrops, outputDrops, dropTableHead, skillNames, infoboxItem, infoboxRecipe)
-    translateItemNames();
+    translateItemNames(outputDrops);
 });
 
 outputDrops.addEventListener('click', () => {
