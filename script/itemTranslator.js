ca859ddd9cde817cf66b06c0762e44d1f5ab6f45
LucianoDLima <ljr.luciano.junior@outlook.com>
2023-06-24 22:02:10 -0300

    Removed one of the symbols to check for translations.
    Since one of the npcs decided it was a good idea to be called literally Name ...

--- script/itemTranslator.js
+++ script/itemTranslator.js
@@ -1,14 +1,13 @@
-import { fetchTranslatedName } from "./items.js";
+import { handleFetchData } from "./items.js";
 
-export const translateItemNames = (outputDrops) => {
+export const translateItemNames = (outputDrops, data) => {
   let lines = outputDrops.value.split('\n');
   const symbolsToCheck = [
       ['=', '|'],
       ['[[', ']'],
       ['Descobrir:', '|'],
       ['[[Arquivo:', '.png'],
-      ['==', '=='],
-      ['|', '}}']
+      ['==', '==']
   ];
 
   for (let i = 0; i < lines.length; i++) {
@@ -16,23 +15,9 @@ export const translateItemNames = (outputDrops) => {
 
       // Check if it has a name to be translated.
       symbolsToCheck.forEach(([start, end]) => {
-          handleFetchItems(item, lines, i, start, end);
+          handleFetchData(item, lines, i, start, end, data);
       });
   }
 
   outputDrops.value = lines.join('\n');
 };
-
-function handleFetchItems(item, lines, i, start, end, fetcher) {
-  if (item.indexOf(start) != -1) {
-    item = item.split(start);
-    item = item[1].split(end);
-    item = item[0].trim();
-
-    const translatedName = fetchTranslatedName(item.toLowerCase());
-    if (translatedName) {
-      // Replace the item name with the translated name.
-      lines[i] = lines[i].replace(item, translatedName);
-    }
-  }
-}
