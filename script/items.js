ce49e3c98a16c0a4c7aa2f8e98c140e2fe024f72
LucianoDLima <ljr.luciano.junior@outlook.com>
2023-06-24 22:01:03 -0300

    Modified so it can call both data files.

--- script/items.js
+++ script/items.js
@@ -1,16 +1,36 @@
+export let itemNames;
+export let npcNames;
+
 try {
-    const response = await fetch('./data/data.json');
-    var itemNames = await response.json(); 
+    const responseItem = await fetch('./data/dataItems.json');
+    itemNames = await responseItem.json(); 
+
+    const responseNpc = await fetch('./data/dataNPCs.json');
+    npcNames = await responseNpc.json(); 
 } catch (error) {
     console.error('Error loading JSON file:', error);
 }
 
-export const fetchTranslatedName = (item) => {
-    const itemNamesLength = itemNames.length;
-    for (let i = 0; i < itemNamesLength; i++) {
-        if (itemNames[i][0].toLowerCase() === item) {
-            return itemNames[i][1];
+const fetchTranslatedName = (item, data) => {
+    const dataLength = data.length;
+    for (let i = 0; i < dataLength; i++) {
+        if (data[i][0].toLowerCase() === item) {
+            return data[i][1];
         }
     }
     return false;
 };
+
+export function handleFetchData(item, lines, i, start, end, data) {
+    if (item.indexOf(start) != -1) {
+      item = item.split(start);
+      item = item[1].split(end);
+      item = item[0].trim();
+  
+      const translatedName = fetchTranslatedName(item.toLowerCase(), data);
+      if (translatedName) {
+        // Replace the item name with the translated name.
+        lines[i] = lines[i].replace(item, translatedName);
+      }
+    }
+  }
