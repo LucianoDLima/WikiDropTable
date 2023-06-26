bbbe6d52e1fae0ab1bec162227c2411ebabdb626
João Pedro Droval <55903030+PvMDragonic@users.noreply.github.com>
2023-06-26 16:14:00 -0300

    Moving functions from items.js to itemTranslator.js.
    It makes more sense to keep all translation related stuff on the same place.

--- script/itemTranslator.js
+++ script/itemTranslator.js
@@ -1,24 +1,41 @@
-import { handleFetchData } from "./items.js";
+function fetchTranslatedName(item, data) {
+    const dataMap = new Map(data);
+    return dataMap.get(item) || false;
+}
 
-export const translateItemNames = (outputDrops, data) => {
-  let lines = outputDrops.value.split('\n');
-  const symbolsToCheck = [
-      ['=', '|'],
-      ['=', '.png'],
-      ['[[', ']'],
-      ['Descobrir:', '|'],
-      ['[[Arquivo:', '.png'],
-      ['==', '==']
-  ];
+function handleFetchData(item, lines, i, start, end, data) {
+    if (item.includes(start)) {
+        // Strips the item name from the table line.
+        item = item.split(start);
+        item = item[1].split(end);
+        item = item[0].trim();
 
-  for (let i = 0; i < lines.length; i++) {
-      let item = lines[i];
+        const translatedName = fetchTranslatedName(item, data);
+        if (translatedName) {
+            lines[i] = lines[i].replace(item, translatedName);
+        }
+    }
+}
 
-      // Check if it has a name to be translated.
-      symbolsToCheck.forEach(([start, end]) => {
-          handleFetchData(item, lines, i, start, end, data);
-      });
-  }
+export function translateItemNames(outputDrops, data) {
+    let lines = outputDrops.value.split('\n');
+    const symbolsToCheck = [
+        ['=', '|'],
+        ['=', '.png'],
+        ['[[', ']'],
+        ['Descobrir:', '|'],
+        ['[[Arquivo:', '.png'],
+        ['==', '==']
+    ];
 
-  outputDrops.value = lines.join('\n');
+    for (let i = 0; i < lines.length; i++) {
+        let item = lines[i];
+
+        // Check if it has a name to be translated.
+        symbolsToCheck.forEach(([start, end]) => {
+            handleFetchData(item, lines, i, start, end, data);
+        });
+    }
+
+    outputDrops.value = lines.join('\n');
 };
--- script/items.js
+++ script/items.js
@@ -10,22 +10,3 @@ try {
 } catch (error) {
     console.error('Error loading JSON file:', error);
 }
-
-function fetchTranslatedName(item, data) {
-    const dataMap = new Map(data);
-    return dataMap.get(item) || false;
-}
-
-export function handleFetchData(item, lines, i, start, end, data) {
-    if (item.includes(start)) {
-        // Strips the item name from the DropsLine.
-        item = item.split(start);
-        item = item[1].split(end);
-        item = item[0].trim();
-
-        const translatedName = fetchTranslatedName(item, data);
-        if (translatedName) {
-            lines[i] = lines[i].replace(item, translatedName);
-        }
-    }
-}
