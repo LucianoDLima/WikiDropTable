fb51d60705774d6d54bb0504f5a89cfaab9c3f9f
João Pedro Droval <55903030+PvMDragonic@users.noreply.github.com>
2023-06-28 16:52:18 -0300

    Parameter translation algorithm rework.
    Too many changes to list, but reworked it from scratch to work with
    a single Map of parameters instead of fractured ones for each infobox.
    
    Also added parameters.json and infoboxes.json to hold their respective
    key-value combinations.

--- script/parameterTranslator.js
+++ script/parameterTranslator.js
@@ -1,75 +1,129 @@
-import * as tp from './parameters.js';
+import { parameters, infoboxes } from './parameters.js';
 
-const monsterAndNpc = {
-    ...tp.infoboxMonster,
-    ...tp.infoboxNPC
-}
+const exceptions = new Map([
+    ['date', dateException],
+    ['release', releaseException]
+]);
 
-const recipeSkillNames = {
-    ...tp.infoboxRecipe,
-    ...tp.skillNames
-}
-
-const shopAndDrop = {
-    ...tp.dropTableHead,
-    ...tp.infoboxShop
-}
-
-const allTemplates = {
-    ...tp.dropTableHead,
-    ...tp.infoboxItem,
-    ...tp.infoboxRecipe,
-    ...tp.skillNames,
-    ...tp.updateHistory,
-    ...tp.infoboxSummoning,
-    ...tp.infoboxMonster,
-    ...tp.infoboxNPC,
-    ...tp.infoboxShop
-}
-
-/**
- * Translates parameters in the input text based on the @parameterToBeTranslated object.
- */
-export function translateParameters(inputText) {
-    switch (true) {
-        case inputText.includes('{{Infobox Monster new'):
-        case inputText.includes('{{Infobox NPC'):
-            return handleParameters(inputText, monsterAndNpc);
-        
-        case inputText.includes('{{Infobox familiar'):
-        case inputText.includes('{{Infobar Summon Pouch'):
-        case inputText.includes('{{Infobox Summon scroll'):
-            return handleParameters(inputText, tp.infoboxSummoning);
-
-        case inputText.includes('{{infobox item'):
-            return handleParameters(inputText, tp.infoboxItem);
-        
-        case inputText.includes('==Creation=='):
-        case inputText.includes('Infobox Recipe'):
-            return handleParameters(inputText, recipeSkillNames);
-            
-        case inputText.includes('===Main drops==='):
-        case inputText.includes('===Secondary drops==='):
-        case inputText.includes('{{DropsTableHead}}'):
-            return handleParameters(inputText, tp.dropTableHead);
-        
-        case inputText.includes('==Update history=='):
-        case inputText.includes('{{UH|'):
-            return handleParameters(inputText, tp.updateHistory);
-
-        case inputText.includes('{{Infobox loja'):
-        case inputText.includes('{{StoreTableHead}}'):
-            return handleParameters(inputText, shopAndDrop);
+function dateException(text) {
+    // Finds all matching patterns of 'date = 12 June 2023'.
+    const exceptionRegexDate = /date\s*=\s*(\d+)\s+([\w\s]+)\s+(\d+)/ig;
+    const matchesDate = text.matchAll(exceptionRegexDate);
+    
+    for (const match of matchesDate) {
+        const [day, month, year] = match;
+        text = text.replace(match[0], `data={{Data|${day}|${month.toLowerCase()}|${year}}}`);
     }
-        
-    return handleParameters(inputText, allTemplates);
+
+    return text;
 }
 
-function handleParameters(inputText, templates) {
-    // Splits by both '<' and '>' via RegEx.
-    let output = inputText.split(/[<>]+/);
+function releaseException(text) {
+    const months = new Map([
+        ["january", "Janeiro"],
+        ["february", "Fevereiro"],
+        ["march", "Março"],
+        ["april", "Abril"],
+        ["may", "Maio"],
+        ["june", "Junho"],
+        ["july", "Julho"],
+        ["august", "Agosto"],
+        ["september", "Setembro"],
+        ["october", "Outubro"],
+        ["november", "Novembro"],
+        ["december", "Dezembro"]
+    ]);
 
-    output = handleParamaterToBeTranslated(output, output.length, templates);
+    // Finds all matching patterns of the 'release' parameter '([Day Month] [Year])'.
+    const exceptionRegexRelease = /release\s*=\s*\[\[([\w\s]+)\]\]\s*\[\[([\w\s]+)\]\]/ig;
+    const matchesRelease = text.matchAll(exceptionRegexRelease);
+
+    for (const match of matchesRelease) {
+        const dayMonth = match[1].trim();
+        const year = match[2].trim();
+        const [day, month] = dayMonth.split(' ');
+        text = text.replace(match[0], `release = {{Data|${day}|${months.get(month.toLowerCase())}|${year}}}`);
+    }
+
+    return text;
+}
+
+export function translateParameters(inputText) {
+    let inputTextLines = inputText.split('\n');
+    const linesLength = inputTextLines.length;
+
+    // Searches the predef/infobox name from the known ones at infoboxes.
+    const predefName = [...infoboxes.keys()].find(key => inputTextLines[0].includes(key)) || false;
+
+    if (predefName) {
+        inputTextLines[0] = inputTextLines[0].replace(predefName, infoboxes.get(predefName));
+    }
+
+    for (let i = 0; i < linesLength; i++) {
+        inputTextLines[i] = handleParameters(inputTextLines[i]);
+    }
+
+    return inputTextLines.join('\n');
+}
+
+function splitLineParameters(paramLine) {
+    let params = paramLine.split(/[|=]+/);
+    if (params.length === 1) {
+        return false;
+    }
+    // Groups paramName and paramValue together, whilist also getting rid of the first element (that is useless).
+    return Array.from({ length: (params.length - 1) / 2 }, (_, i) => [params[i * 2 + 1].trim(), params[i * 2 + 2].trim()]);
+}
+
+function handleParameters(inputText) {
+    // Tries to split by both '<' and '>' via RegEx.
+    let output = inputText.split(/[<>]+/) || [output];
+    const outputLength = output.length;
+
+    for (let i = 0; i < outputLength; i++) {
+        if (i % 2 === 0) {
+            // Handles lines with multiple param-value combinations.
+            const allParams = splitLineParameters(output[i]);
+
+            if (!allParams) {
+                continue;
+            }
+            
+            for (const [paramName, paramValue] of allParams) {
+                if (exceptions.has(paramName)) {
+                    // 'exceptions.get(paramName)' is the function that handles the entire 'output[i]'.
+                    output[i] = exceptions.get(paramName)(output[i]);
+                    continue;
+                } 
+                
+                if (parameters.has(paramName)) {
+                    output[i] = output[i].replace(paramName, parameters.get(paramName));
+                }
+
+                if (paramValue != null) {
+                    // Splits by both ', ' and ':' to catch multiple values or [[File]].
+                    let splittedParamValue = paramValue.split(/, |:/);
+
+                    for (const elem of splittedParamValue) {
+                        let lowercase = elem.toLowerCase();
+
+                        // Handles having }} at the end of the line.
+                        if (lowercase.endsWith('}}')) {
+                            lowercase = lowercase.replace('}}', '');
+                            if (parameters.has(lowercase)) {
+                                output[i] = output[i].replace(elem, `${parameters.get(lowercase)}}}`);
+                            }
+                            continue;
+                        }
+
+                        if (parameters.has(lowercase)) {      
+                            output[i] = output[i].replace(elem, parameters.get(lowercase));
+                        }
+                    }
+                }
+            }
+        }
+    }
 
     // Joins 'output' by alternating between angle brackets.
     return output.reduce((acc, curr, index) => {
@@ -79,47 +133,3 @@ function handleParameters(inputText, templates) {
         return `${acc}${index % 2 === 0 ? '>' : '<'}${curr}`;
     }, '');
 };
-
-function handleParamaterToBeTranslated(output, outputLength, paramaterToBeTranslated) {
-    for (let parameter in paramaterToBeTranslated) {
-        // Insensitive case regex that matches any digits at the end of the string.
-        const regex = new RegExp(`\\b${parameter}\\b\\d*`, 'ig');
-
-        // Extract any digits at the end of the match and append them to the replacement string.
-        const digits = parameter.match(/\d+$/);
-        const translated = paramaterToBeTranslated[parameter] + (digits ? digits[0] : '');
-
-        for (let i = 0; i < outputLength; i++) {
-            if (i % 2 === 0) {
-                // Finds all matching patterns of 'date = 12 June 2023'.
-                let exceptionRegexDate = /date\s*=\s*(\d+)\s+([\w\s]+)\s+(\d+)/ig;
-                let matchesDate = output[i].matchAll(exceptionRegexDate);
-                
-                for (let match of matchesDate) {
-                    const [day, month, year] = match;
-                    const exceptionReplacementDate = `data={{Data|${day}|${month.toLowerCase()}|${year}}}`;
-                    output[i] = output[i].replace(match[0], exceptionReplacementDate);
-                }
-
-                // Finds all matching patterns of the 'release' parameter '([Day Month] [Year])'.
-                let exceptionRegexRelease = /release\s*=\s*\[\[([\w\s]+)\]\]\s*\[\[([\w\s]+)\]\]/ig;
-                let matchesRelease = output[i].matchAll(exceptionRegexRelease);
-
-                for (let match of matchesRelease) {
-                    const dayMonth = match[1].trim();
-                    const year = match[2].trim();
-                    const [day, month] = dayMonth.split(' ');
-                    const exceptionReplacementRelease = `release = {{Data|${day}|${month.toLowerCase()}|${year}}}`;
-                    output[i] = output[i].replace(match[0], exceptionReplacementRelease);
-                }
-
-                // If no exceptions are found, perform regular translation.
-                if (matchesDate.length == null && matchesRelease.length == null) {
-                    output[i] = output[i].replace(regex, translated);
-                }
-            }
-        }
-    }
-
-    return output;
-}
