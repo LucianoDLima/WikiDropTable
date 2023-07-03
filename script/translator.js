96a88c321805476d95fc6d764fb9b207092e2c47
João Pedro Droval <55903030+PvMDragonic@users.noreply.github.com>
2023-07-03 11:36:31 -0300

    Support for numbered parameters.
    Params like 'image1' will be properly handled now.
    
    Also added some new comments and changed the code structure
    to better divide each code block into its own function.

--- script/translator.js
+++ script/translator.js
@@ -52,6 +52,7 @@ function translatePredefNames(inputText) {
 }
 
 function splitParamsFromLine(paramLine) {
+    // Splits by both '|' and '=' to separate all params in a line.
     let params = paramLine.split(/[|=]+/);
 
     return params.length === 1 ? false : Array.from(
@@ -63,15 +64,98 @@ function splitParamsFromLine(paramLine) {
         ]);
 }
 
-function handleParameters(inputTextLine) {
+function handleParamName(paramName, outputLine) {
+    const lowercase = paramName.toLowerCase();
+            
+    if (exceptions.has(lowercase)) {
+        return exceptions.get(lowercase)(outputLine);
+    }
+
+    if (parameters.has(lowercase)) {
+        return outputLine.replace(`${paramName}=`, `${parameters.get(lowercase)}=`);
+    }
+    
+    // Handles parameters with numbers, like 'image1='.
+    const versionNumber = lowercase.slice(-1);
+    if (!isNaN(versionNumber)) {
+        const withoutNumber = lowercase.slice(0, -1);
+        if (parameters.has(withoutNumber)) {
+            return outputLine.replace(`${paramName}=`, `${parameters.get(withoutNumber)}${versionNumber}=`);
+        }
+    }
+
+    return outputLine;
+}
+
+function handleParamValue(paramValue, outputLine) {
+    // Splits by ':', '(' and ', ' to catch [[File]], (Notes) and multiple values.
+    const paramValues = paramValue.split(/, |:|\(/);
+
+    for (const elem of paramValues) {
+        let lowercase = elem.toLowerCase();
+
+        const lastChar = lowercase.slice(-1);
+        
+        // Handles (Noted) and other variants.
+        if (lastChar === ')') {
+            lowercase = lowercase.slice(0, -1);
+            if (parameters.has(lowercase)) {
+                outputLine = outputLine.replace(`${elem}`, `${parameters.get(lowercase)})`);
+                continue;
+            }
+        }
+
+        // Handles {{DropsLine}} and such, that end with }}.
+        if (lastChar === '}') {
+            lowercase = lowercase.slice(0, -2);
+            if (parameters.has(lowercase)) {
+                outputLine = outputLine.replace(`=${elem}`, `=${parameters.get(lowercase)}}}`);
+                continue;
+            }
+        }
+
+        if (parameters.has(lowercase)) {
+            outputLine = outputLine.replace(`=${elem}`, `=${parameters.get(lowercase)}`);
+            continue;
+        }
+
+        // Most likely, it's an item name if it made it here.
+        if (itemNames.has(lowercase)) {
+            outputLine = outputLine.replace(elem, itemNames.get(lowercase));
+            continue;
+        }
+
+        // Past here, it'll try to scavenge the string to remove any gunk.
+        const scavengedString = paramValuesTrie.removeSymbols(elem);
+        lowercase = scavengedString.toLowerCase();
+
+        // Sometimes, stuff like (melee) on image names will make it down here.
+        if (parameters.has(lowercase)) {
+            outputLine = outputLine.replace(scavengedString, parameters.get(lowercase));
+            continue;
+        }
+
+        if (itemNames.has(lowercase)) {
+            outputLine = outputLine.replace(scavengedString, itemNames.get(lowercase));
+            continue;
+        }
+
+        if (npcNames.has(lowercase)) {
+            outputLine = outputLine.replace(scavengedString, npcNames.get(lowercase));
+        }
+    }
+
+    return outputLine;
+}
+
+function handleInputLine(inputTextLine) {
     // Standardizes to properly replace whole words during paramValue replacing.
-    let output = inputTextLine.replace(/ = /g, "=");
-
-    // Tries to split by both '<' and '>' via RegEx.
-    output = output.split(/[<>]+/);
-
+    // Also tries to split by both '<' and '>' to skip refNotes.
+    const output = inputTextLine.replace(/ = /g, "=").split(/[<>]+/);
     const outputLength = output.length;
+    
     for (let i = 0; i < outputLength; i++) {
+        // If there were any refNotes, they'll be left on odd indexes.
         if (i % 2 === 1) {
             continue;
         }
@@ -85,76 +169,9 @@ function handleParameters(inputTextLine) {
         }
 
         for (const [paramName, paramValue] of allParams) {
-            let lowercase = paramName.toLowerCase();
-            
-            if (exceptions.has(lowercase)) {
-                // 'exceptions.get(lowercase)' is the function that handles the entire 'output[i]'.
-                output[i] = exceptions.get(lowercase)(output[i]);
-                continue;
-            }
-
-            if (parameters.has(lowercase)) {
-                output[i] = output[i].replace(`${paramName}=`, `${parameters.get(lowercase)}=`);
-            }
-
-            // Splits by ':', '|' and ', ' to catch [[File]], (Notes) and multiple values.
-            const paramValues = paramValue.split(/, |:|\(/);
-
-            for (const elem of paramValues) {
-                lowercase = elem.toLowerCase();
-
-                const lastChar = lowercase.slice(-1);
-                
-                // Handles (Noted) and other variants.
-                if (lastChar === ')') {
-                    lowercase = lowercase.slice(0, -1);
-                    if (parameters.has(lowercase)) {
-                        output[i] = output[i].replace(`${elem}`, `${parameters.get(lowercase)})`);
-                        continue;
-                    }
-                }
-
-                // Handles {{DropsLine}} and such, that end with }}.
-                if (lastChar === '}') {
-                    lowercase = lowercase.slice(0, -2);
-                    if (parameters.has(lowercase)) {
-                        output[i] = output[i].replace(`=${elem}`, `=${parameters.get(lowercase)}}}`);
-                        continue;
-                    }
-                }
-
-                if (parameters.has(lowercase)) {
-                    output[i] = output[i].replace(`=${elem}`, `=${parameters.get(lowercase)}`);
-                    continue;
-                }
-
-                // Most likely, it's an item name if it made it here.
-                if (itemNames.has(lowercase)) {
-                    output[i] = output[i].replace(elem, itemNames.get(lowercase));
-                    continue;
-                }
-
-                // Past here, it'll try to scavenge the string to remove any gunk.
-                const scavengedString = paramValuesTrie.removeSymbols(elem);
-                lowercase = scavengedString.toLowerCase();
-
-                // Sometimes, stuff like (melee) on image names will make it down here.
-                if (parameters.has(lowercase)) {
-                    output[i] = output[i].replace(scavengedString, parameters.get(lowercase));
-                    continue;
-                }
-
-                if (itemNames.has(lowercase)) {
-                    output[i] = output[i].replace(scavengedString, itemNames.get(lowercase));
-                    continue;
-                }
-
-                if (npcNames.has(lowercase)) {
-                    output[i] = output[i].replace(scavengedString, npcNames.get(lowercase));
-                    continue;
-                }
-            }
-        }    
+            output[i] = handleParamName(paramName, output[i]);
+            output[i] = handleParamValue(paramValue, output[i]);
+        }
     }
 
     // Joins 'output' by alternating between angle brackets.
@@ -162,7 +179,7 @@ function handleParameters(inputTextLine) {
 }
 
 export function translate(inputText) {
-    // Needs to be splitted by newlines to catch the end of a predef/infobox.
-    inputText = inputText.split('\n').map(handleParameters).join('\n');
+    // Needs to split by newlines to catch the end of a predef/infobox.
+    inputText = inputText.split('\n').map(handleInputLine).join('\n');
     return translatePredefNames(inputText);
 }
