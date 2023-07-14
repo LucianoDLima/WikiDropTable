e52704a6a48ed5be60055a7c0fa4bd2807808e9a
João Pedro Droval <55903030+PvMDragonic@users.noreply.github.com>
2023-07-14 16:39:05 -0300

    Adding a proper copy button for the output.
    Also removed the old copy, which activated by clicking anywhere in the
    output's textarea.

--- script/script.js
+++ script/script.js
@@ -2,13 +2,41 @@ import { translate } from './translator.js';
 
 const inputTranslator = document.querySelector('#input');
 const outputTranslator = document.querySelector('#output');
+const copyButton = document.getElementById('copy-button');
+const copyIcon = document.getElementById('copy-icon');
+const copySucess = document.getElementById('copy-success');
 
 inputTranslator.addEventListener('input', () => {
-    outputTranslator.value = translate(inputTranslator.value);
+    const translated = translate(inputTranslator.value);
+    outputTranslator.value = translated;
+
+    if (translated) {
+        // Moves it to the side if there's a scrollbar active.
+        copyButton.classList.toggle(
+            'copy-button--active-scroll', 
+            outputTranslator.clientHeight < outputTranslator.scrollHeight
+        );
+        
+        copyButton.classList.remove('hidden');
+    } else {
+        copyButton.classList.add('hidden');
+    }
+
 });
 
-outputTranslator.addEventListener('click', () => {
-    navigator.clipboard.writeText(outputDrops.value).catch((error) => {
-        console.error('Failed to copy text: ', error);
-    });
+copyButton.addEventListener('focusout', () => {
+    copySucess.classList.add('hidden');
+    copyIcon.classList.remove('hidden');
+});
+
+copyButton.addEventListener('click', () => {
+    navigator.clipboard
+        .writeText(outputTranslator.value)
+        .then(() => {
+            copyIcon.classList.add('hidden');
+            copySucess.classList.remove('hidden');
+        })
+        .catch((error) => {
+            console.error('Failed to copy text: ', error);
+        });
 });
